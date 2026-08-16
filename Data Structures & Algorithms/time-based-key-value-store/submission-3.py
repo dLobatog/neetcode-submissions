935232c61ef15ff446d0621d5352a87bd13fbefd
Daniel Lobato García <me@daniellobato.me>
2026-08-16 09:57:35 -0400

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,30 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keys = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.keys[key].append((value, timestamp)) # since we know timestamps are set strictly increasing 
+        
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.keys[key]
+        # find timestamp_prev s.t timestamp_prev <= timestamp (maximize timestamp_prev)  
+        l, r = 0, len(values)-1
+        mid = (l + r)//2
+        while l <= r:
+            if values[mid][1] > timestamp:
+                r = mid-1
+            elif values[mid][1] < timestamp:
+                # move right because timestamp < value but we want the highest one
+                l = mid + 1
+            else:
+                return values[mid][0]
+
+            mid = (l + r)//2
+
+        if mid >= 0 and mid < len(values) and values[mid][1] <= timestamp:
+            return values[mid][0]
+        else:
+            return ""
+        
