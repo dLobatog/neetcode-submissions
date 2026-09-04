815174fd2101024298849ff9b80fb3aa4ee1bdbd
Daniel Lobato García <me@daniellobato.me>
2026-09-04 06:48:52 -0400

    Add: pow-x-n - submission-8

---
+++ Data Structures & Algorithms/pow-x-n/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # we could split the n in 2 constantly?
+        if n == 0:
+            return 1.0
+        
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+
+        half = self.myPow(x, n // 2)
+        
+        if n % 2 == 0:
+            return half * half 
+        else: 
+            return half * half * x
+        
